c2276da0c1246968c1a0c8c65ec3eab3861cb598
Jorman <jorman.iisrael@me.com>
2021-12-28 15:02:06 -0500

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    plugins:[
+        new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery"
+        }),
+      ],
+    entry:'./assets/js/script.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'main.bundle.js'
+    },
+    mode:'development'
+};
